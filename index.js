4af24106ad8a6385f12ae438d24f5a35ff4c52ad
cwarett2 <cwarett2@gmail.com>
2025-09-16 21:47:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'https://cwarett1.netlify.app'
+    'https://cwarett.com'
   ],
   credentials: true
 }));
@@ -367,5 +367,6 @@ app.get('/api/health', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
